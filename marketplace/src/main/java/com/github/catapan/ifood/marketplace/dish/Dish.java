a34d4eedf6fa08b7627c9b01a8bf18a8536780d2
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-04-12 13:06:05 -0300

    feat: improve findAll from Dish class

--- marketplace/src/main/java/com/github/catapan/ifood/marketplace/dish/Dish.java
+++ marketplace/src/main/java/com/github/catapan/ifood/marketplace/dish/Dish.java
@@ -1,7 +1,14 @@
 package com.github.catapan.ifood.marketplace.dish;
 
 import com.github.catapan.ifood.marketplace.restaurant.Restaurant;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.pgclient.PgPool;
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowSet;
+import io.vertx.mutiny.sqlclient.Tuple;
 import java.math.BigDecimal;
+import java.util.stream.StreamSupport;
 
 public class Dish {
 
@@ -14,4 +21,24 @@ public class Dish {
     public Restaurant restaurant;
 
     public BigDecimal price;
+
+    public static Multi<DishDTO> findAll(PgPool pgPool) {
+        Uni<RowSet<Row>> preparedQuery = pgPool.query("select * from dish").execute();
+        return unitToMulti(preparedQuery);
+    }
+
+    public static Multi<DishDTO> findAll(PgPool client, Long idRestaurant) {
+        Uni<RowSet<Row>> preparedQuery = client
+          .preparedQuery("SELECT * FROM dish where dish.restaurant_id = $1 ORDER BY name ASC").execute(
+            Tuple.of(idRestaurant));
+        return unitToMulti(preparedQuery);
+    }
+
+    private static Multi<DishDTO> unitToMulti(Uni<RowSet<Row>> queryResult) {
+        return queryResult.onItem()
+          .produceMulti(set -> Multi.createFrom().items(() -> {
+              return StreamSupport.stream(set.spliterator(), false);
+          }))
+          .onItem().apply(DishDTO::from);
+    }
 }
