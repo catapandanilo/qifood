7a707d69ee84ddcdc42082d27207e8de3d741f61
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-04-12 13:23:28 -0300

    feat: improve APIResponse findAll from DishResource

--- marketplace/src/main/java/com/github/catapan/ifood/marketplace/dish/DishResource.java
+++ marketplace/src/main/java/com/github/catapan/ifood/marketplace/dish/DishResource.java
@@ -8,6 +8,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 @Path("/dishes")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -18,7 +22,12 @@ public class DishResource {
   PgPool pgPool;
 
   @GET
+  @APIResponse(
+	responseCode = "200",
+	content = @Content(schema= @Schema(type = SchemaType.ARRAY, implementation = DishDTO.class))
+)
   public Multi<DishDTO> getDishes() {
     return Dish.findAll(pgPool);
   }
+
 }
