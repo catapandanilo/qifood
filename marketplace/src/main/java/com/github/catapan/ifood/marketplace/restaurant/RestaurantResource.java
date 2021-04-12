14be9b7e62ac1d8616455fa5c78dc8dc34a623d6
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-04-12 13:07:45 -0300

    feat: add RestaurantResource

---
+++ marketplace/src/main/java/com/github/catapan/ifood/marketplace/restaurant/RestaurantResource.java
@@ -0,0 +1,29 @@
+package com.github.catapan.ifood.marketplace.restaurant;
+
+import com.github.catapan.ifood.marketplace.dish.Dish;
+import com.github.catapan.ifood.marketplace.dish.DishDTO;
+import io.smallrye.mutiny.Multi;
+import io.vertx.mutiny.pgclient.PgPool;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+@Path("restaurants")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RestaurantResource {
+
+  @Inject
+  PgPool pgPool;
+
+  @GET
+  @Path("{idRestaurant}/dishes")
+  public Multi<DishDTO> getDishes(@PathParam("idRestaurant") Long idRestaurant) {
+    return Dish.findAll(pgPool, idRestaurant);
+  }
+
+}
