504b24c974a136481b9027f7f9b57e0c374e9d69
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-04-13 09:38:43 -0300

    feat: add class ShoppingCart

---
+++ marketplace/src/main/java/com/github/catapan/ifood/marketplace/ShoppingCart.java
@@ -0,0 +1,75 @@
+package com.github.catapan.ifood.marketplace;
+
+import com.github.catapan.ifood.marketplace.dish.Dish;
+import com.github.catapan.ifood.marketplace.dish.DishDTO;
+import com.github.catapan.ifood.marketplace.dish.DishOrderDTO;
+import com.github.catapan.ifood.marketplace.restaurant.RestaurantDTO;
+import io.smallrye.mutiny.Uni;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+@Path("cart")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ShoppingCart {
+
+    private static final String CLIENT = "a";
+
+    @Inject
+    io.vertx.mutiny.pgclient.PgPool pgPool;
+
+    @Inject
+    @Channel("orders")
+    Emitter<OrderRealizedDTO> emitterOrder;
+
+    @GET
+    public Uni<List<DishCart>> getCart() {
+        return DishCart.findCart(pgPool, CLIENT);
+    }
+
+    @POST
+    @Path("/dish/{idDish}")
+    public Uni<Long> addDish(@PathParam("idDish") Long idDish) {
+        //poderia retornar o pedido atual
+        DishCart pc = new DishCart();
+        pc.client = CLIENT;
+        pc.dish = idDish;
+
+        return DishCart.save(pgPool, CLIENT, idDish);
+    }
+
+    @POST
+    @Path("/close-order")
+    public Uni<Boolean> closeOrder() {
+        OrderRealizedDTO order = new OrderRealizedDTO();
+        String client = CLIENT;
+        order.client = client;
+        List<DishCart> dishCarts = DishCart.findCart(pgPool, client).await().indefinitely();
+        //Utilizar mapstruts
+        order.dishes = dishCarts.stream().map(pc -> from(pc)).collect(Collectors.toList());
+
+        RestaurantDTO restaurant = new RestaurantDTO();
+        restaurant.name = "name restaurant";
+        order.restaurant = restaurant;
+
+        emitterOrder.send(order);
+
+        return DishCart.delete(pgPool, client);
+    }
+
+    private DishOrderDTO from(DishCart dishCart) {
+        DishDTO dto = Dish.findById(pgPool, dishCart.dish).await().indefinitely();
+        return new DishOrderDTO(dto.name, dto.description, dto.price);
+    }
+
+}
