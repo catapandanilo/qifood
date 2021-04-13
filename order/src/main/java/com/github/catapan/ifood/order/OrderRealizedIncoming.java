174f4aa6b899959c2240b078f36159564eb885cf
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-04-13 10:53:07 -0300

    feat: add OrderRealizedIncoming

---
+++ order/src/main/java/com/github/catapan/ifood/order/OrderRealizedIncoming.java
@@ -0,0 +1,41 @@
+package com.github.catapan.ifood.order;
+
+import com.github.catapan.ifood.order.dish.Dish;
+import com.github.catapan.ifood.order.dish.DishOrderDTO;
+import com.github.catapan.ifood.order.dish.DishOrderRealizedDTO;
+import com.github.catapan.ifood.order.restaurant.Restaurant;
+import java.util.ArrayList;
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.bind.JsonbBuilder;
+import org.bson.types.Decimal128;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+@ApplicationScoped
+public class OrderRealizedIncoming  {
+
+    @Incoming("orders")
+    public void readOrders(DishOrderRealizedDTO dto) {
+        System.out.println("-----------------");
+        System.out.println(dto);
+
+        Order order = new Order();
+        order.client = dto.client;
+        order.dishes = new ArrayList<>();
+        dto.dishes.forEach(dish -> order.dishes.add(from(dish)));
+        Restaurant restaurant = new Restaurant();
+        restaurant.name = dto.restaurant.name;
+        order.restaurant = restaurant;
+        String json = JsonbBuilder.create().toJson(dto);
+        order.persist();
+
+    }
+
+    private Dish from(DishOrderDTO dish) {
+        Dish newDish = new Dish();
+        newDish.description = dish.description;
+        newDish.name = dish.name;
+        newDish.price = new Decimal128(dish.price);
+        return newDish;
+    }
+
+}
